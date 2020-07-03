6e56509e785363ae89871ac04001fb12f88fb3c3
Sekkappan Ramasamy <sekkappanramasamy@gmail.com>
2020-07-03 16:12:55 +1000

    Modified for terraform

---
+++ terraform/initializeTF.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env sh
+
+terraform workspace select dev_env || terraform workspace new dev_env
+terraform init
+    # -backend-config="bucket=$PROJECT_NAME-tf-state" \
+    # -backend-config="prefix=terraform/state" \
+    # -backend-config="credentials=../secret/service-account.json"
