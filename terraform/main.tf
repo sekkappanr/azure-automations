a1105501a7855857ce72c3e3119d47aad446941d
sekkappanr <sekkappanramasamy@gmail.com>
2020-10-01 19:01:16 +1000

    initial Commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,9 @@
 # Configure the Azure provider
 provider "azurerm" {
     version = "~>1.32.0"
+    use_msi = true
     subscription_id = "2720979b-7976-4d8b-b3a4-17d451a9f460"
     tenant_id       = "e0793d39-0939-496d-b129-198edd916feb"
-    features {}
 }
 
 # Create a new resource group
