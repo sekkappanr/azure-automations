58bf590f0f7773367782b0189047eaf08ab383c2
sekkappanr <sekkappanramasamy@gmail.com>
2020-10-02 15:46:23 +1000

    terraform

--- terraform/network_security_group/networksecuritygroup.tf
+++ terraform/network_security_group/networksecuritygroup.tf
@@ -1,12 +1,12 @@
 ############### The below module config is defined to create ec2 related security groups. ####################
-module "sg" {
+module "nsg" {
         source = "../../root/modules/network_security_group/"
         security_group_name=format("%s-vm",var.security_group_name)
-        resource_name_prefix=var.resource_name_prefix
+        resource_name_prefix="${var.resource_name_prefix}"
         ipv4_ingress_cidr_block=["10.0.0.0/8"]
         custom_irules = "${var.custom_irules}"
         create_sg = length(var.custom_irules) > 0 ? true : false
-        standard_tags = var.standard_tags
-        custom_tags = var.custom_tags
+        standard_tags = "${var.standard_tags}"
+        custom_tags = "${var.custom_tags}"
         custom_irules_source = var.custom_irules_source
 }
