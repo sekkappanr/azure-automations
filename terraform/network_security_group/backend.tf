6e80a8c2ca9a49341106bc1cba7dbb4aac8cad12
sekkappanr <sekkappanramasamy@gmail.com>
2020-10-02 15:36:24 +1000

    terraform

--- terraform/network_security_group/backend.tf
+++ terraform/network_security_group/backend.tf
@@ -3,6 +3,5 @@ terraform {
         resource_group_name = "${var.rg_tf_state}"
         storage_account_name = "${var.storage_tf_state}"
         container_name = "${var.container_tf_state}"
-        location = "${var.az_region}"
     }
 }
