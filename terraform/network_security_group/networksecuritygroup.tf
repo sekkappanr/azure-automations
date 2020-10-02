23cc800b54d335141d50e614db046714d5c083bf
sekkappanr <sekkappanramasamy@gmail.com>
2020-10-02 16:29:10 +1000

    terraform

--- terraform/network_security_group/networksecuritygroup.tf
+++ terraform/network_security_group/networksecuritygroup.tf
@@ -4,7 +4,7 @@ module "nsg" {
         security_group_name=format("%s-vm",var.security_group_name)
         resource_name_prefix=var.resource_name_prefix
         resource_group_name=azurerm_resource_group.application_resource_group.name
-        az_region=${var.az_region}
+        az_region="${var.az_region}"
         ipv4_ingress_cidr_block=["10.0.0.0/8"]
         custom_irules = "${var.custom_irules}"
         create_sg = length(var.custom_irules) > 0 ? true : false
