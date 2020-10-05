8aac41701fa8cda1d786560390cd6f3d7b120b2d
sekkappanr <sekkappanramasamy@gmail.com>
2020-10-05 23:41:32 +1100

    added LB

--- root/modules/vmset_lb/instance.tf
+++ root/modules/vmset_lb/instance.tf
@@ -18,13 +18,13 @@ resource "azurerm_public_ip" "az_public_ip" {
   resource_group_name = var.resource_group_name
   allocation_method   = "Static"
   sku                 = "Standard"
-  domain_name_label   = format("vm_%s_pip0${count.index + 1}", lower(replace(var.resource_name_prefix[o], "/[[:^alnum:]]/", "")))
+  domain_name_label   = format("vm_%s_pip0${count.index + 1}", lower(replace(var.resource_name_prefix[0], "/[[:^alnum:]]/", "")))
 
-  tags                = merge({ "ResourceName" = lower("pip-vm-${var.resource_name_prefix[0]}-${var.az_region}-0${count.index + 1}") }, var.tags, )
+  #tags                = merge({ "ResourceName" = lower("pip-vm-${var.resource_name_prefix[0]}-${var.az_region}-0${count.index + 1}") }, var.tags, )
 
 }
 
-resource "azurerm_lb" "tf_lb" {
+resource "azurerm_lb" "az_public_sslb" {
     count = var.enable_load_balancer == true && var.load_balancer_type == "public" ? 1 : 0
     name                = length(format("%s%s-%s",var.resource_name_prefix[3],var.resource_name_tag,random_string.lb_rstring[count.index].result)) > 32 ? substr(format("%s%s-%s",var.resource_name_prefix[3],var.resource_name_tag,random_string.lb_rstring[count.index].result),0,31) : format("%s%s-%s",var.resource_name_prefix[3],var.resource_name_tag,random_string.lb_rstring[count.index].result)
     location            = var.location
@@ -35,7 +35,7 @@ resource "azurerm_lb" "tf_lb" {
     frontend_ip_configuration {
         name                          = var.load_balancer_type == "public" ? lower("lbext-frontend-${var.resource_name_prefix[0]}") : lower("lbint-frontend-${var.resource_name_prefix[0]}")
 
-        public_ip_address_id          = var.enable_load_balancer == true && var.load_balancer_type == "public" ? azurerm_public_ip.pip[count.index].id : null
+        public_ip_address_id          = var.enable_load_balancer == true && var.load_balancer_type == "public" ? azurerm_public_ip.az_public_ip[count.index].id : null
 
         private_ip_address_allocation = var.load_balancer_type == "private" ? var.private_ip_address_allocation : null
         private_ip_address            = var.load_balancer_type == "private" && var.private_ip_address_allocation == "Static" ? var.lb_private_ip_address : null
