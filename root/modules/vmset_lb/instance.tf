06fad4ffd52d68c38e2c9b6655cc52e63a06b7f5
sekkappanr <sekkappanramasamy@gmail.com>
2020-10-05 23:38:21 +1100

    added LB

--- root/modules/vmset_lb/instance.tf
+++ root/modules/vmset_lb/instance.tf
@@ -7,6 +7,23 @@ data "azurerm_image" "tfd_image" {
      resource_group_name=var.resource_group_name
 }
 
+#-----------------------------------
+# Create the public IP for Azure Load Balancer
+#-----------------------------------
+resource "azurerm_public_ip" "az_public_ip" {
+  count               = var.enable_load_balancer == true && var.load_balancer_type == "public" ? 1 : 0
+  name                = lower("pbl-ip-vm-${var.resource_name_prefix[0]}-${var.az_region}-0${count.index + 1}")
+
+  location            = var.az_region
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  domain_name_label   = format("vm_%s_pip0${count.index + 1}", lower(replace(var.resource_name_prefix[o], "/[[:^alnum:]]/", "")))
+
+  tags                = merge({ "ResourceName" = lower("pip-vm-${var.resource_name_prefix[0]}-${var.az_region}-0${count.index + 1}") }, var.tags, )
+
+}
+
 resource "azurerm_lb" "tf_lb" {
     count = var.enable_load_balancer == true && var.load_balancer_type == "public" ? 1 : 0
     name                = length(format("%s%s-%s",var.resource_name_prefix[3],var.resource_name_tag,random_string.lb_rstring[count.index].result)) > 32 ? substr(format("%s%s-%s",var.resource_name_prefix[3],var.resource_name_tag,random_string.lb_rstring[count.index].result),0,31) : format("%s%s-%s",var.resource_name_prefix[3],var.resource_name_tag,random_string.lb_rstring[count.index].result)
@@ -23,7 +40,7 @@ resource "azurerm_lb" "tf_lb" {
         private_ip_address_allocation = var.load_balancer_type == "private" ? var.private_ip_address_allocation : null
         private_ip_address            = var.load_balancer_type == "private" && var.private_ip_address_allocation == "Static" ? var.lb_private_ip_address : null
 
-        subnet_id                     = var.load_balancer_type == "private" ? data.azurerm_subnet.az_subnet.id : null
+        #subnet_id                     = var.load_balancer_type == "private" ? data.azurerm_subnet.az_subnet.id : null
     }
 }
 
@@ -40,34 +57,6 @@ resource "azurerm_lb_backend_address_pool" "az_lb_backend_pool" {
 
 
 
-
- resource "random_string" "elb_rstring" {
-        count = var.type_elb == "classic" ? 1 : 0
-        length = 16
-        upper = true
-        lower = false
-        number = true
-        special = false
-}
-
-resource "random_string" "asg_lstring" {
-        count =  var.number_of_servers
-        length = 8
-        upper = true
-        lower = false
-        number = true
-        special = false
-}
-
-resource "random_string" "asg_rstring" {
-        count = var.number_of_servers
-        length = 8
-        upper = true
-        lower = false
-        number = true
-        special = false
-}
-
 resource "random_string" "lb_rstring" {
         count = var.load_balancer_type == "private" || var.load_balancer_type == "public" ? local.no_of_lb :  0
         length = 8
@@ -77,22 +66,5 @@ resource "random_string" "lb_rstring" {
         special = false
 }
 
-resource "random_string" "tg_rstring" {
-        count = var.type_elb == "network" || var.type_elb == "application" ? local.no_of_target_groups :  0
-        length = 8
-        upper = true
-        lower = false
-        number = true
-        special = false
-}
-
-resource "random_string" "tg_lstring" {
-        count = var.type_elb == "network" || var.type_elb == "application" ? local.no_of_target_groups :  0
-        length = 8
-        upper = true
-        lower = false
-        number = true
-        special = false
-}
 
  
